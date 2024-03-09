ecf87c2785f5ba3f049d4723224455a746b02600
tutu martinss <oliveira.arthur@ime.eb.br>
2024-03-09 17:41:49 -0300

    questao 11

--- kdkdkd.py
+++ kdkdkd.py
@@ -1,34 +1,30 @@
-p = 1j*1j
+import matplotlib.pyplot as plt
 
-q = p*1j
-
-r = q*1j
-
-t = r*1j
-
-#iremos imprimir agora o valor de t e constatar que eh igual a 
+import numpy as np
 
 
+labels = ['Z1','Z1 x Ro']
+str1 = input("digite um complexo da forma a+bj ")
 
-#dessa forma, vemos que pow(i,3)= pow(i,7)= pow(i,11)= pow(i,15)
+z1 = complex(str1)
+z2 = z1*(z1.real)
+print(" ",z2)
+complex_numbers = [z1,z2]
 
+real_part = [num.real for num in complex_numbers]
+imaginary_part = [num.imag for num in complex_numbers]
 
-#assim temos que: 
-n = int(input("digite o valor de n"))
-
-
-if(n%4 == 0):
-    
-    print(" ",r)
-    
-if(n%4 ==1):
-    print(" ",1j)  
-    
-if(n%4 ==2):
-    
-    print(" ",p)   
+for label, x, y in zip(labels, real_part, imaginary_part):
+    plt.text(x, y, label)
+plt.scatter(real_part,imaginary_part,color = 'blue',)
+plt.xlabel('Parte Real')
+plt.ylabel('Parte Imaginária')
 
 
-if(n%4 == 3):
-    
-    print("",q)
+plt.title('Gráfico de Números Complexos')
+
+
+plt.grid(True)
+plt.axvline(0, color='black',linewidth=0.5)
+plt.show()
+
